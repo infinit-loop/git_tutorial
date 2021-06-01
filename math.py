826c2717264e71da5ac7a89b6d4773c7e4323621
infinit-loop <utkarsh.sanwal56@gmail.com>
2021-06-01 11:24:36 +0530

    implement type checking

--- math.py
+++ math.py
@@ -1,6 +1,8 @@
 # I need to implement this
 def add(x,y):
-  return x+y
+	if(type(x) == int and type(y) == int):
+  		return x+y
+	return None
 #another needs to implement this
 def mult(x,y):
   return None
